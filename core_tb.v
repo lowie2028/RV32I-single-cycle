42a82f75f3b3dd9648a5010a6154cac8ec306969
lowie <lowie.deferme@student.uhasselt.be>
2020-12-15 12:06:47 +0100

    edited testbench

--- core_tb.v
+++ core_tb.v
@@ -7,7 +7,7 @@ module core_tb;
     parameter IO_BASE_ADDR = 'h60;
 
     reg clock, reset;
-    wire [IO_INPUT_BUS_LEN-1:0] io_input_bus;
+    reg [IO_INPUT_BUS_LEN-1:0] io_input_bus;
     wire [IO_OUTPUT_BUS_LEN-1:0] io_output_bus;
 
     core #(
@@ -25,8 +25,30 @@ module core_tb;
     always #5 clock <= ~clock;
 
     initial begin
+        // Set to known state
         clock <= 0;
         reset <= 1;
         #10 reset <=0;
+        // Set inputs                       // --------------------
+        #20 io_input_bus[13:10] = 'b0101;   // |13 10|9          0|
+        io_input_bus[9:0] = 'b001001110;    // | KEY |     SW     |
+        // Stop simulation
+        #370 $stop();
     end
+
+    // Simulation output (inspired by Luc's example)
+    always begin // Print header line every 20 lines
+        $display ("|Time [   PC   ] Instruct |                     Outputs (IO)                     |  Inputs  (IO)  |");
+        $display ("|-------------------------|------------------------------------------------------|----------------|");
+        #200 $display ("");
+    end
+
+    always 
+        #10 $display ("|%4d [%8h] %8h | %52b | %14b |", 
+            ($time/10), 
+            DUT.IF.pc_out, 
+            DUT.instruction,
+            io_output_bus,
+            io_input_bus
+        );
 endmodule
